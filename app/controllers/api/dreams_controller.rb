217ffeed36d2e6c4526a59b754da261761f58df5
alecgalba <alecgalba@gmail.com>
2017-12-21 16:03:56 -0500

    built dream controller under api namespace

---
+++ app/controllers/api/dreams_controller.rb
@@ -0,0 +1,47 @@
+class API::DreamsController < ApplicationController
+  before_action :find_dream, only: [:show, :update, :destroy]
+
+  def index
+    render json: Dream.all
+  end
+
+  def show
+    render json: @dream
+  end
+
+  def create
+    dream = Dream.new(dream_params)
+    if dream.save
+      render json: dream
+    else
+      render json: {message: dream.errors}, status: 400
+    end
+  end
+
+  def update
+    if @dream.update(dream_params)
+      render json: @dream
+    else
+      render json: {message: dream.errors}, status: 400
+    end
+  end
+
+  def destroy
+    if @dream.destroy
+      render status: 204
+    else
+      render json: {message: "Unable to forget this dream"}, status: 400
+    end
+  end
+
+  private
+
+    def find_dream
+      @dream = Dream.find_by(id: params[:id])
+    end
+
+    def dream_params
+      params.require(:dream)permit(:name, :description, :sleep_hours, :likes)
+    end
+
+end
