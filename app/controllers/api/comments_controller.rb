de43a107d34954f556f90cb21bb354684149b5ac
alecgalba <alecgalba@gmail.com>
2017-12-27 11:53:44 -0500

    added comment migration and set controller actions

---
+++ app/controllers/api/comments_controller.rb
@@ -0,0 +1,21 @@
+class CommentsController < ApplicationController
+
+  def index
+    render json: Comment.all
+  end
+
+  def create
+    comment = Comment.new(comment_params)
+    if comment.save
+      render json: comment
+    else
+      render json: {message: comment.errors}, status: 400
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:author, :comment)
+  end
+end
